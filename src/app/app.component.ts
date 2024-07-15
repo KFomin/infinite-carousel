319ca82c8e87fef839f759a05f90f2f9518bbeac
KFomin <fominkonstantiin@gmail.com>
2024-07-16 00:18:36 +0300

    add base templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {InfiniteCarouselComponent} from "./components/infinite-carousel/infinite-carousel.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, InfiniteCarouselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
