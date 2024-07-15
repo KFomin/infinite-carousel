d36be204b5facad679d2faebcbfcef5e8d3bf9cf
KFomin <fominkonstantiin@gmail.com>
2024-07-16 00:50:48 +0300

    add test data

---
+++ src/app/test-data/offers.ts
@@ -0,0 +1,20 @@
+import {IOffer} from "../data/IOffer";
+
+export const offers: IOffer[] = [
+  {
+    title: "WinzUp Loyalty Program",
+    description: "Get up to 35% in rewards: daily rakeback, weekly cashback and level-up bonuses",
+    imgUrl: "assets/loyalty_bonuses_offer.png"
+  },
+  {
+    title: " Valentine's Fortune Drops",
+    description: "Trigger random prizes and win a share of €30,000!",
+    imgUrl: "assets/lucky_wheel_offer.png"
+  },
+  {
+    title: " Wheel of Winz",
+    description: "Spin the wheel to win up to €15,000 weekly",
+    imgUrl: "assets/valentines_offer.png.png"
+  }
+]
+
